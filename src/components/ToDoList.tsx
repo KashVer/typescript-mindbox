592867b8e174e59526eeeb37f2fa4c617cdd2cf4
KashVer <veronika.kashkadova55@gmail.com>
2023-06-12 18:14:47 +0300

    minor changes

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -24,7 +24,7 @@ export const ToDoList: React.FC<IToDoListProps> = ({ toDos, onToggle }) => {
                 className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
               />
               <label
-                className={`w-full py-3 mx-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize break-normal ${
+                className={`w-full py-3 mx-2 text-sm font-medium text-gray-900 dark:text-gray-300 break-normal ${
                   toDo.completed && "line-through"
                 }`}
               >
